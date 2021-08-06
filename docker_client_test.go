e6d285d3d30a065d741589defe19a9cf786fed16
Nicolas Duchon <nicolas.duchon@gmail.com>
2021-08-06 11:20:06 +0200

    tests: use github.com/stretchr/testify/assert

--- docker_client_test.go
+++ docker_client_test.go
@@ -2,195 +2,130 @@ package dockergen
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSplitDockerImageRepository(t *testing.T) {
 	registry, repository, tag := splitDockerImage("ubuntu")
 
-	if registry != "" {
-		t.Fail()
-	}
-	if repository != "ubuntu" {
-		t.Fail()
-	}
-	if tag != "" {
-		t.Fail()
-	}
+	assert.Equal(t, "", registry)
+	assert.Equal(t, "ubuntu", repository)
+	assert.Equal(t, "", tag)
 
 	dockerImage := DockerImage{
 		Registry:   registry,
 		Repository: repository,
 		Tag:        tag,
 	}
-	if "ubuntu" != dockerImage.String() {
-		t.Fail()
-	}
+	assert.Equal(t, "ubuntu", dockerImage.String())
 }
 
 func TestSplitDockerImageWithRegistry(t *testing.T) {
 	registry, repository, tag := splitDockerImage("custom.registry/ubuntu")
 
-	if registry != "custom.registry" {
-		t.Fail()
-	}
-	if repository != "ubuntu" {
-		t.Fail()
-	}
-	if tag != "" {
-		t.Fail()
-	}
+	assert.Equal(t, "custom.registry", registry)
+	assert.Equal(t, "ubuntu", repository)
+	assert.Equal(t, "", tag)
+
 	dockerImage := DockerImage{
 		Registry:   registry,
 		Repository: repository,
 		Tag:        tag,
 	}
-	if "custom.registry/ubuntu" != dockerImage.String() {
-		t.Fail()
-	}
-
+	assert.Equal(t, "custom.registry/ubuntu", dockerImage.String())
 }
 
 func TestSplitDockerImageWithRegistryAndTag(t *testing.T) {
 	registry, repository, tag := splitDockerImage("custom.registry/ubuntu:12.04")
 
-	if registry != "custom.registry" {
-		t.Fail()
-	}
-	if repository != "ubuntu" {
-		t.Fail()
-	}
-	if tag != "12.04" {
-		t.Fail()
-	}
+	assert.Equal(t, "custom.registry", registry)
+	assert.Equal(t, "ubuntu", repository)
+	assert.Equal(t, "12.04", tag)
+
 	dockerImage := DockerImage{
 		Registry:   registry,
 		Repository: repository,
 		Tag:        tag,
 	}
-	if "custom.registry/ubuntu:12.04" != dockerImage.String() {
-		t.Fail()
-	}
-
+	assert.Equal(t, "custom.registry/ubuntu:12.04", dockerImage.String())
 }
 
 func TestSplitDockerImageWithRepositoryAndTag(t *testing.T) {
 	registry, repository, tag := splitDockerImage("ubuntu:12.04")
 
-	if registry != "" {
-		t.Fail()
-	}
+	assert.Equal(t, "", registry)
+	assert.Equal(t, "ubuntu", repository)
+	assert.Equal(t, "12.04", tag)
 
-	if repository != "ubuntu" {
-		t.Fail()
-	}
-
-	if tag != "12.04" {
-		t.Fail()
-	}
 	dockerImage := DockerImage{
 		Registry:   registry,
 		Repository: repository,
 		Tag:        tag,
 	}
-	if "ubuntu:12.04" != dockerImage.String() {
-		t.Fail()
-	}
+	assert.Equal(t, "ubuntu:12.04", dockerImage.String())
 }
 
 func TestSplitDockerImageWithPrivateRegistryPath(t *testing.T) {
 	registry, repository, tag := splitDockerImage("localhost:8888/ubuntu/foo:12.04")
 
-	if registry != "localhost:8888" {
-		t.Fail()
-	}
+	assert.Equal(t, "localhost:8888", registry)
+	assert.Equal(t, "ubuntu/foo", repository)
+	assert.Equal(t, "12.04", tag)
 
-	if repository != "ubuntu/foo" {
-		t.Fail()
-	}
-
-	if tag != "12.04" {
-		t.Fail()
-	}
 	dockerImage := DockerImage{
 		Registry:   registry,
 		Repository: repository,
 		Tag:        tag,
 	}
-	if "localhost:8888/ubuntu/foo:12.04" != dockerImage.String() {
-		t.Fail()
-	}
+	assert.Equal(t, "localhost:8888/ubuntu/foo:12.04", dockerImage.String())
 }
 func TestSplitDockerImageWithLocalRepositoryAndTag(t *testing.T) {
 	registry, repository, tag := splitDockerImage("localhost:8888/ubuntu:12.04")
 
-	if registry != "localhost:8888" {
-		t.Fatalf("registry does not match: expected %s got %s", "localhost:8888", registry)
-	}
+	assert.Equal(t, "localhost:8888", registry)
+	assert.Equal(t, "ubuntu", repository)
+	assert.Equal(t, "12.04", tag)
 
-	if repository != "ubuntu" {
-		t.Fatalf("repository does not match: expected %s got %s", "ubuntu", repository)
-	}
-
-	if tag != "12.04" {
-		t.Fatalf("tag does not match: expected %s got %s", "12.04", tag)
-	}
 	dockerImage := DockerImage{
 		Registry:   registry,
 		Repository: repository,
 		Tag:        tag,
 	}
-	if "localhost:8888/ubuntu:12.04" != dockerImage.String() {
-		t.Fail()
-	}
-
+	assert.Equal(t, "localhost:8888/ubuntu:12.04", dockerImage.String())
 }
 
 func TestParseHostUnix(t *testing.T) {
 	proto, addr, err := parseHost("unix:///var/run/docker.sock")
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	if proto != "unix" || addr != "/var/run/docker.sock" {
-		t.Fatal("failed to parse unix:///var/run/docker.sock")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, "unix", proto, "failed to parse unix:///var/run/docker.sock")
+	assert.Equal(t, "/var/run/docker.sock", addr, "failed to parse unix:///var/run/docker.sock")
 }
 
 func TestParseHostUnixDefault(t *testing.T) {
 	proto, addr, err := parseHost("")
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	if proto != "unix" || addr != "/var/run/docker.sock" {
-		t.Fatal("failed to parse ''")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, "unix", proto, "failed to parse ''")
+	assert.Equal(t, "/var/run/docker.sock", addr, "failed to parse ''")
 }
 
 func TestParseHostUnixDefaultNoPath(t *testing.T) {
 	proto, addr, err := parseHost("unix://")
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	if proto != "unix" || addr != "/var/run/docker.sock" {
-		t.Fatal("failed to parse unix://")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, "unix", proto, "failed to parse unix://")
+	assert.Equal(t, "/var/run/docker.sock", addr, "failed to parse unix://")
 }
 
 func TestParseHostTCP(t *testing.T) {
 	proto, addr, err := parseHost("tcp://127.0.0.1:4243")
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	if proto != "tcp" || addr != "127.0.0.1:4243" {
-		t.Fatal("failed to parse tcp://127.0.0.1:4243")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, "tcp", proto, "failed to parse tcp://127.0.0.1:4243")
+	assert.Equal(t, "127.0.0.1:4243", addr, "failed to parse tcp://127.0.0.1:4243")
 }
 
 func TestParseHostTCPDefault(t *testing.T) {
 	proto, addr, err := parseHost("tcp://:4243")
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	if proto != "tcp" || addr != "127.0.0.1:4243" {
-		t.Fatal("failed to parse unix:///var/run/docker.sock")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, "tcp", proto, "failed to parse tcp://:4243")
+	assert.Equal(t, "127.0.0.1:4243", addr, "failed to parse tcp://:4243")
 }

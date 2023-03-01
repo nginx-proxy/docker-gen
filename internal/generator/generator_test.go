c8cfb0bbad6949a1d24084948f63b6fe4923f3bf
Richard Hansen <rhansen@rhansen.org>
2023-03-01 18:59:07 -0500

    tests: Slow down generator tests to work around race condition
    5ms resolution is too fine for reliable tests.  Increase delays by a
    factor of 10 to improve the chances that the desired path will win the
    race.
    
    Ideally the test would be rewritten to remove the races entirely, but
    I'll leave that as a future exercise.

--- internal/generator/generator_test.go
+++ internal/generator/generator_test.go
@@ -39,7 +39,7 @@ func TestGenerateFromEvents(t *testing.T) {
 		for rsc.Scan() {
 			w.Write([]byte(rsc.Text()))
 			w.(http.Flusher).Flush()
-			time.Sleep(15 * time.Millisecond)
+			time.Sleep(150 * time.Millisecond)
 		}
 		time.Sleep(500 * time.Millisecond)
 	}))
@@ -166,13 +166,13 @@ func TestGenerateFromEvents(t *testing.T) {
 					Template: tmplFile.Name(),
 					Dest:     destFiles[2].Name(),
 					Watch:    true,
-					Wait:     &config.Wait{Min: 20 * time.Millisecond, Max: 25 * time.Millisecond},
+					Wait:     &config.Wait{Min: 200 * time.Millisecond, Max: 250 * time.Millisecond},
 				},
 				{
 					Template: tmplFile.Name(),
 					Dest:     destFiles[3].Name(),
 					Watch:    true,
-					Wait:     &config.Wait{Min: 25 * time.Millisecond, Max: 100 * time.Millisecond},
+					Wait:     &config.Wait{Min: 250 * time.Millisecond, Max: 1 * time.Second},
 				},
 			},
 		},
@@ -189,12 +189,12 @@ func TestGenerateFromEvents(t *testing.T) {
 
 	// The counter is incremented in each output file in the following sequence:
 	//
-	//       init   0ms    5ms    10ms   15ms   20ms   25ms   30ms   35ms   40ms   45ms   50ms   55ms
+	//       init   150ms  200ms  250ms  300ms  350ms  400ms  450ms  500ms  550ms  600ms  650ms  700ms
 	//       ├──────╫──────┼──────┼──────╫──────┼──────┼──────╫──────┼──────┼──────┼──────┼──────┤
 	// File0 ├─ 1   ║                    ║                    ║
 	// File1 ├─ 1   ╟─ 2                 ╟─ 3                 ╟─ 5
-	// File2 ├─ 1   ╟───── max (25ms) ───║───────────> 4      ╟─────── min (20ms) ──────> 6
-	// File3 └─ 1   ╟──────────────────> ╟──────────────────> ╟─────────── min (25ms) ─────────> 7
+	// File2 ├─ 1   ╟───── max (250ms) ──║───────────> 4      ╟─────── min (200ms) ─────> 6
+	// File3 └─ 1   ╟──────────────────> ╟──────────────────> ╟─────────── min (250ms) ────────> 7
 	//          ┌───╨───┐            ┌───╨──┐             ┌───╨───┐
 	//          │ start │            │ stop │             │ start │
 	//          └───────┘            └──────┘             └───────┘

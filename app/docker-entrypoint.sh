9f755809e4a25abcb15f5d369c3f69d757801070
Nicolas Duchon <nicolas.duchon@gmail.com>
2026-07-13 16:02:15 +0200

    fix: regression introduced in #761

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -5,15 +5,15 @@ set -eu
 # If no arguments are passed, default to running docker-gen
 if [ "$#" -eq 0 ]; then
 	set -- docker-gen
-fi
-
-# Prepend docker-gen unless $1 resolves to an executable regular file
-_cmd="$(command -v -- "$1" 2>/dev/null)"
-if [ -f "$_cmd" ] && [ -x "$_cmd" ]; then
-	:
 else
-	set -- docker-gen "$@"
+	# Prepend docker-gen unless $1 resolves to an executable regular file
+	_cmd=''
+	if _cmd="$(command -v -- "$1" 2>/dev/null)" && [ -f "$_cmd" ] && [ -x "$_cmd" ]; then
+		:
+	else
+		set -- docker-gen "$@"
+	fi
+	unset _cmd
 fi
-unset _cmd
 
 exec "$@"

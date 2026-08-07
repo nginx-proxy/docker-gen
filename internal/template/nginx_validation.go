750c653fe25a50d84f24a9095170cbb0a44af0ee
Nicolas Duchon <nicolas.duchon@gmail.com>
2026-08-08 01:35:10 +0200

    feat: clarify nginxMustParseHSTS error messages

--- internal/template/nginx_validation.go
+++ internal/template/nginx_validation.go
@@ -26,36 +26,36 @@ func nginxMustParseHSTS(value string) (string, error) {
 		directive = strings.TrimSpace(directive)
 		if maxAgeStr, ok := strings.CutPrefix(directive, "max-age="); ok {
 			if maxAgeStr == "" {
-				return "", fmt.Errorf("invalid max-age directive: %s", directive)
+				return "", fmt.Errorf("invalid Strict-Transport-Security max-age directive: %s", directive)
 			}
 			for _, r := range maxAgeStr {
 				if r < '0' || r > '9' {
-					return "", fmt.Errorf("invalid max-age directive: %s", directive)
+					return "", fmt.Errorf("invalid Strict-Transport-Security max-age directive: %s", directive)
 				}
 			}
 			_, err := fmt.Sscanf(maxAgeStr, "%d", &directives.MaxAge)
 			if err != nil {
-				return "", fmt.Errorf("invalid max-age directive: %s", directive)
+				return "", fmt.Errorf("invalid Strict-Transport-Security max-age directive: %s", directive)
 			}
 		} else if directive == "includesubdomains" {
 			directives.IncludeSubDomains = true
 		} else if directive == "preload" {
 			directives.Preload = true
 		} else {
-			return "", fmt.Errorf("unknown HSTS directive: %s", directive)
+			return "", fmt.Errorf("unknown Strict-Transport-Security directive: %s", directive)
 		}
 	}
 
 	if directives.MaxAge <= 0 {
-		return "", fmt.Errorf("max-age directive must be greater than 0")
+		return "", fmt.Errorf("Strict-Transport-Security max-age directive must be greater than 0")
 	}
 
 	if directives.Preload {
 		if !directives.IncludeSubDomains {
-			return "", fmt.Errorf("preload directive requires includeSubDomains to be set")
+			return "", fmt.Errorf("Strict-Transport-Security preload directive requires includeSubDomains to be set")
 		}
 		if directives.MaxAge < 31536000 {
-			return "", fmt.Errorf("preload directive requires max-age to be at least 31536000 seconds (1 year)")
+			return "", fmt.Errorf("Strict-Transport-Security preload directive requires max-age to be at least 31536000 seconds (1 year)")
 		}
 		return fmt.Sprintf("max-age=%d; includeSubDomains; preload", directives.MaxAge), nil
 	}
